9dbbce9f90876e5523a2edd401f613c72da31198
Duc TO. Nguyen Minh <ducnm11@techcombank.com.vn>
2022-12-23 14:45:01 +0700

    Change: - Resolve package dependencies

--- ComponentSystemTests/ComponentSystemTests.swift
+++ ComponentSystemTests/ComponentSystemTests.swift
@@ -6,7 +6,7 @@
 //
 
 import XCTest
-@testable import ComponentSystem
+@testable import DesignCore
 
 class ComponentSystemTests: XCTestCase {
 
--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ import PackageDescription
 let package = Package(
     name: "ComponentSystem",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -44,6 +44,9 @@ let package = Package(
         ),
         .target(
             name: "DesignToolbox",
+            dependencies: [
+                .target(name: "DesignCore")
+            ],
             path: "Sources/Design"
         ),
         .target(
