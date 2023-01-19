127e923454a5fc31f09db778dbb98f04e2c94020
Duc TO. Nguyen Minh <ducnm11@techcombank.com.vn>
2023-01-19 18:29:42 +0700

    Change: - Update Button

--- Sources/Core/Exts/UIButton+.swift
+++ Sources/Core/Exts/UIButton+.swift
@@ -20,11 +20,31 @@ public extension UIControl {
     ///   - closure: Things happen
     func addAction(for controlEvent: UIControl.Event = .touchUpInside, _ closure: @escaping() -> Void) {
         if #available(iOS 14.0, *) {
-            addAction(UIAction { (_: UIAction) in closure() }, for: controlEvent)
+            let action = UIAction { (_: UIAction) in closure() }
+            addAction(action, for: controlEvent)
+            return action.identifier.rawValue
         } else {
             let sleeve = ClosureSleeve(closure)
+            let identifier = "\(UUID())"
             addTarget(sleeve, action: #selector(ClosureSleeve.invoke), for: controlEvent)
-            objc_setAssociatedObject(self, "\(UUID())", sleeve, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+            objc_setAssociatedObject(self, identifier, sleeve, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+            return identifier
+        }
+    }
+    
+    func removeAction(for controlEvent: UIControl.Event = .touchUpInside, identifier: String? = nil) {
+        guard let identifier = identifier else {
+            // FIXME: Remove all actions for controlEvent if identifier is now provided
+            return
+        }
+        if #available(iOS 14.0, *) {
+            removeAction(identifiedBy: .init(rawValue: identifier), for: controlEvent)
+        } else {
+            guard let sleeve = objc_getAssociatedObject(self, identifier) as? ClosureSleeve else {
+                // FIXME: Throw an error here if it is not found or we can just ignore like removeAction?
+                return
+            }
+            removeTarget(sleeve, action: #selector(ClosureSleeve.invoke), for: controlEvent)
         }
     }
 }
