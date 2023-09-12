8b054cf228cf37040dbb1eb6ae96bfbefe55c907
Duc TO. Nguyen Minh <ducnm11@techcombank.com.vn>
2023-09-12 09:41:01 +0700

    Change: - Update Chaining Builder

--- Sources/Core/Exts/Builder.swift
+++ Sources/Core/Exts/Builder.swift
@@ -7,12 +7,31 @@
 
 import Foundation
 
-public protocol Chainable { }
+public protocol Chainable: SelfCustomizable { }
 
 public extension Chainable where Self: AnyObject {
     @discardableResult
     func with<T>(_ property: ReferenceWritableKeyPath<Self, T>, setTo value: T) -> Self {
         self[keyPath: property] = value
         return self
+    }
+}
+
+public protocol SelfCustomizable {
+    func custom(_ configuration: (inout Self) -> Void) -> Self
+    func updated<Value>(with value: Value, for keyPath: WritableKeyPath<Self, Value>) -> Self
+}
+
+public extension SelfCustomizable {
+    func custom(_ configuration: (inout Self) -> Void) -> Self {
+        var newSelf = self
+        configuration(&newSelf)
+        return newSelf
+    }
+    
+    func updated<Value>(with value: Value, for keyPath: WritableKeyPath<Self, Value>) -> Self {
+        var newSelf = self
+        newSelf[keyPath: keyPath] = value
+        return newSelf
     }
 }
