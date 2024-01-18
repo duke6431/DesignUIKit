088cc00e3be9618c5299e0392c65493a9adf6836
Duc TO. Nguyen Minh <ducnm11@techcombank.com.vn>
2024-01-18 16:40:24 +0700

    Change: Unify detecting system

---
+++ Sources/UI/Exts/Device+.swift
@@ -0,0 +1,54 @@
+//
+//  File.swift
+//  
+//
+//  Created by Duc IT. Nguyen Minh on 18/01/2024.
+//
+
+#if os(iOS)
+import SwiftUI
+#endif
+import Foundation
+
+public enum DeviceSystem: Int, Codable, @unchecked Sendable {
+    
+    case unspecified = -1
+    
+    case phone = 0
+    
+    case pad = 1
+    
+    case tv = 2
+    
+    case carPlay = 3
+    
+    case mac = 5
+    
+    case vision = 6 // Vision UI
+    
+    public static var current: DeviceSystem {
+#if os(macOS)
+        return .mac
+#elseif os(iOS)
+        switch UIDevice.current.userInterfaceIdiom {
+        case .phone:
+            return .phone
+        case .pad:
+            return .pad
+        case .tv:
+            return .tv
+        case .carPlay:
+            return .carPlay
+        case .mac:
+            return .mac
+        @unknown default:
+            if #available(iOS 17.0, *) {
+                if UIDevice.current.userInterfaceIdiom == .vision {
+                    return .vision
+                }
+            }
+            return .unspecified
+        }
+#endif
+    }
+}
