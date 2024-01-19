1814220783750c2482b33c50d58a6db536b35fd6
Duc TO. Nguyen Minh <ducnm11@techcombank.com.vn>
2024-01-19 11:12:33 +0700

    Change: - Update View Conditioner

--- Sources/UI/Exts/View+Conditioner.swift
+++ Sources/UI/Exts/View+Conditioner.swift
@@ -12,14 +12,41 @@ public extension View {
     /// - Parameters:
     ///   - condition: The condition to evaluate.
     ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` according to the condition.
+    @ViewBuilder
+    func `if`<Content: View>(_ condition: @autoclosure () -> Bool, transform: (Self) -> Content) -> some View {
+        if condition() {
+            transform(self)
+        } else {
+            self
+        }
+    }
+    
+    /// Applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View`.
     ///   - else: The other transform to apply to the source `View`.
     /// - Returns: Either the original `View` or the modified `View` according to the condition.
     @ViewBuilder
-    func `if`<Content: View, OtherContent: View>(_ condition: @autoclosure () -> Bool, transform: (Self) -> Content, else othertransform: ((Self) -> OtherContent)? = nil) -> some View {
+    func `if`<Content: View, OtherContent: View>(_ condition: @autoclosure () -> Bool, transform: (Self) -> Content, else othertransform: ((Self) -> OtherContent)) -> some View {
         if condition() {
             transform(self)
-        } else if let othertransform {
+        } else {
             othertransform(self)
+        }
+    }
+    
+    /// Applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - optional: Optional value as a condition.
+    ///   - transform: The transform to apply to the source `View`.
+    ///   - else: The other transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` according to the optional value.
+    @ViewBuilder
+    func `if`<T, Content: View>(_ optional: T?, transform: (Self, T) -> Content) -> some View {
+        if let optional {
+            transform(self, optional)
         } else {
             self
         }
@@ -32,13 +59,11 @@ public extension View {
     ///   - else: The other transform to apply to the source `View`.
     /// - Returns: Either the original `View` or the modified `View` according to the optional value.
     @ViewBuilder
-    func `if`<T, Content: View, OtherContent: View>(_ optional: T?, transform: (Self, T) -> Content, else othertransform: ((Self) -> OtherContent)? = nil) -> some View {
+    func `if`<T, Content: View, OtherContent: View>(_ optional: T?, transform: (Self, T) -> Content, else othertransform: ((Self) -> OtherContent)) -> some View {
         if let optional {
             transform(self, optional)
-        } else if let othertransform {
-            othertransform(self)
         } else {
-            self
+            othertransform(self)
         }
     }
     
