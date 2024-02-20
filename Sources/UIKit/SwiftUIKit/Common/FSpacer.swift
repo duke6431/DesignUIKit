942758aaa8ca7b17f72504232f4b5450cab0db3b
Duc Nguyen <ducnguyen6431@outlook.com>
2024-02-20 22:43:17 +0700

    Change: - Add blur effect to FSpacer

--- Sources/UIKit/SwiftUIKit/Common/FSpacer.swift
+++ Sources/UIKit/SwiftUIKit/Common/FSpacer.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 public final class FSpacer: FBase<UIView>, FComponent {
+    public var blurStyle: UIBlurEffect.Style?
     public var customConfiguration: ((UIView, FSpacer) -> UIView)?
 
     public init(width: CGFloat? = nil, height: CGFloat? = nil) {
@@ -20,8 +21,21 @@ public final class FSpacer: FBase<UIView>, FComponent {
     public override func rendered() -> UIView {
         var view = UIView()
         backgroundColor = contentBackgroundColor
+        if !UIAccessibility.isReduceTransparencyEnabled, let blurStyle {
+            let blurEffectView = UIVisualEffectView(effect: UIBlurEffect(style: blurStyle))
+            view.addSubview(blurEffectView)
+            blurEffectView.snp.makeConstraints {
+                $0.edges.equalToSuperview()
+            }
+        }
         view = customConfiguration?(view, self) ?? view
         content = view
         return view
+    }
+}
+
+public extension FSpacer {
+    func blurred(_ style: UIBlurEffect.Style) -> Self {
+        with(\.blurStyle, setTo: style)
     }
 }
