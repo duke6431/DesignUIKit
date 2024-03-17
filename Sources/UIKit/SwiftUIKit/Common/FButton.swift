a563fc35d034b432a1ed46b3359edcbaba03c147
Duc Nguyen <ducnguyen6431@outlook.com>
2024-03-17 18:37:17 +0700

    Change: - Reupdate FButton layout

--- Sources/UIKit/SwiftUIKit/Common/FButton.swift
+++ Sources/UIKit/SwiftUIKit/Common/FButton.swift
@@ -37,7 +37,6 @@ public class FButton: BaseButton, FConfigurable, FComponent, FStylable, FContent
         self.init(style: .system)
         addAction(for: .touchUpInside, action)
         label().forEach { label in
-            let label = label.attachToParent(false)
             addSubview(label)
             label.isUserInteractionEnabled = false
             if label as? (any FComponent & UIView) == nil {
