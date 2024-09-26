d5fd4f58b97d9bfbaa8c5bf9920c18c39d9af20f
DukeNguyen <ducnguyen6431@outlook.com>
2024-09-26 07:59:21 +0700

    Change: - Add Mac catalyst support

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     name: "ComponentSystem",
     platforms: [
         .iOS(.v13),
-        .macOS(.v14)
+        .macCatalyst(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
