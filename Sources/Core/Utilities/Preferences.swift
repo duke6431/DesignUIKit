43ee1fc7025130f7099523be82fc5c82ece21ebb
DukeNguyen <ducnguyen6431@outlook.com>
2024-10-16 16:14:43 +0700

    Change: - Update UserDefault Handle

--- Sources/Core/Utilities/Preferences.swift
+++ Sources/Core/Utilities/Preferences.swift
@@ -7,27 +7,31 @@
 
 import Foundation
 
+public protocol PreferenceKey {
+    var rawValue: String { get }
+}
+
 @propertyWrapper
 public struct PreferenceItem<T> {
-    let key: String
+    let key: PreferenceKey
     let defaultValue: T
     let calculatedValue: ((T) -> (T))?
 
     public var wrappedValue: T {
         get {
-            let udValue = UserDefaults.standard.value(forKey: key)
-            let res = udValue as? T ?? defaultValue
-            if udValue == nil {
-                UserDefaults.standard.set(defaultValue, forKey: key)
+            guard let res = UserDefaults.standard.value(forKey: key.rawValue) as? T
+            else {
+                UserDefaults.standard.set(defaultValue, forKey: key.rawValue)
+                return calculatedValue?(defaultValue) ?? defaultValue
             }
             return calculatedValue?(res) ?? res
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: key)
+            UserDefaults.standard.set(newValue, forKey: key.rawValue)
         }
     }
 
-    public init(_ key: String, _ defaultValue: T, _ calculatedValue: ((T) -> (T))? = nil) {
+    public init(_ key: PreferenceKey, _ defaultValue: T, _ calculatedValue: ((T) -> (T))? = nil) {
         self.key = key
         self.defaultValue = defaultValue
         self.calculatedValue = calculatedValue
@@ -36,34 +40,32 @@ public struct PreferenceItem<T> {
 
 @propertyWrapper
 public struct PreferenceData<T: Codable> {
-    let key: String
+    let key: PreferenceKey
     let defaultValue: T
     let calculatedValue: ((T) -> (T))?
 
     public var wrappedValue: T {
         get {
-            guard let decoded = UserDefaults.standard.data(forKey: key)
-            else { return defaultValue }
-
-            let decoder = JSONDecoder()
-            guard let res = try? decoder.decode(T.self, from: decoded)
-            else { return defaultValue }
-            
+            guard let decoded = UserDefaults.standard.data(forKey: key.rawValue),
+                  let res = try? JSONDecoder().decode(T.self, from: decoded)
+            else {
+                if let encodedData = try? JSONEncoder().encode(defaultValue) {
+                    UserDefaults.standard.set(encodedData, forKey: key.rawValue)
+                }
+                return calculatedValue?(defaultValue) ?? defaultValue
+            }
             return calculatedValue?(res) ?? res
         }
         set {
-            let userDefaults = UserDefaults.standard
-            let encoder = JSONEncoder()
-
-            if let encodedData: Data = try? encoder.encode(newValue) {
-                userDefaults.set(encodedData, forKey: key)
+            if let encodedData: Data = try? JSONEncoder().encode(newValue) {
+                UserDefaults.standard.set(encodedData, forKey: key.rawValue)
             } else {
-                userDefaults.set(nil, forKey: key)
+                UserDefaults.standard.set(nil, forKey: key.rawValue)
             }
         }
     }
 
-    public init(_ key: String, _ defaultValue: T, calculatedValue: ((T) -> (T))? = nil) {
+    public init(_ key: PreferenceKey, _ defaultValue: T, calculatedValue: ((T) -> (T))? = nil) {
         self.key = key
         self.defaultValue = defaultValue
         self.calculatedValue = calculatedValue
