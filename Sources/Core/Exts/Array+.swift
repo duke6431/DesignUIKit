a0363502d5a0c6e2a41287270e9141e187d5e0ab
DukeNguyen <ducnguyen6431@outlook.com>
2024-12-19 14:21:39 +0700

    Change: - Add Additional support to CommonAttributedString

--- Sources/Core/Exts/Array+.swift
+++ Sources/Core/Exts/Array+.swift
@@ -15,4 +15,8 @@ public extension Array {
         guard index > -1, index < count else { return nil }
         return self[index]
     }
+
+    func insert(separator: Element) -> [Element] {
+        (0 ..< 2 * count - 1).map { $0 % 2 == 0 ? self[$0/2] : separator }
+    }
 }
