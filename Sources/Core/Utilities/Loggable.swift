67abd4f9e2e7682498289ceb9075c0c7540f21c0
DukeNguyen <duke@castalk.com>
2025-06-12 22:36:36 +0700

    Change: - Fix isolation

--- Sources/Core/Utilities/Loggable.swift
+++ Sources/Core/Utilities/Loggable.swift
@@ -11,17 +11,21 @@
 import Foundation
 import Logging
 
+@globalActor public actor LoggingActor {
+    public static let shared: LoggingActor = .init()
+}
+
 /// A protocol that provides a logger instance for conforming types.
 public protocol Loggable {
     /// A logger used for logging messages.
-    var logger: Logger { get }
+    nonisolated var logger: Logger { get }
 }
 
 public extension Loggable {
     /// A default logger instance for the conforming type.
-    static var logger: Logger { Logger(label: String(reflecting: type(of: self))).custom { $0.logLevel = .default } }
+    nonisolated static var logger: Logger { Logger(label: String(reflecting: type(of: self))).custom { $0.logLevel = .default } }
     /// A default logger instance for the conforming type.
-    var logger: Logger { Logger(label: String(reflecting: type(of: self))).custom { $0.logLevel = .default } }
+    nonisolated var logger: Logger { Logger(label: String(reflecting: type(of: self))).custom { $0.logLevel = .default } }
 }
 
 /// Extension to provide a default log level for `Logger.Level`.
